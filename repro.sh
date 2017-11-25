e6799f14214b4fb07f6fef3f6d0a8cd906049a13
Morten Linderud <morten@linderud.pw>
2017-11-25 21:14:22 +0100

    earnestly changes and fixups
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- repro.sh
+++ repro.sh
@@ -1,63 +1,65 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-readonly BUILD_DIRECTORY="$(pwd)/builds"
+readonly build_directory=$PWD/builds
 
-readonly BOOTSTRAP_MIRROR="https://mirror.archlinux.no/iso/latest"
-readonly BOOTSTRAP_IMG="archlinux-bootstrap-$(date +"%Y.%m").01-x86_64.tar.gz"
+readonly bootstrap_mirror=https://mirror.archlinux.no/iso/latest
+readonly bootstrap_img=archlinux-bootstrap-"$(date +%Y.%m)".01-"$(uname -m)".tar.gz
 
 exec_nspawn(){
-    container="$1" && shift 1
-    systemd-nspawn -q \
-        --as-pid2 \
-        -D $BUILD_DIRECTORY/$container "$@"
+    local container=$1
+    systemd-nspawn -q --as-pid2 -D "$build_directory/$container" "${@:2}"
 }
 
-init_container(){
-    if [ ! -d $BUILD_DIRECTORY/root ]; then
-        tar xvf $1 -C $BUILD_DIRECTORY
-        mv $BUILD_DIRECTORY/root.* $BUILD_DIRECTORY/root
+set -e
 
-        host_mirror=$(pacman --cachedir /doesnt/exist -Sddp extra/devtools 2>/dev/null | sed -r 's#(.*/)extra/os/.*#\1$repo/os/$arch#')
-        echo "Server = $host_mirror" >"$BUILD_DIRECTORY/root/etc/pacman.d/mirrorlist"
-        sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n "${cache_dirs[@]}")|g" -i "$BUILD_DIRECTORY/root/etc/pacman.conf"
-        printf '%s.UTF-8 UTF-8\n' en_US de_DE > "$BUILD_DIRECTORY/root/etc/locale.gen"
-        echo 'LANG=en_US.UTF-8' > "$BUILD_DIRECTORY/root/etc/locale.conf"
-        cp "./makepkg.conf" "$BUILD_DIRECTORY/root/etc/makepkg.conf"
-        cp "./pacman.conf" "$BUILD_DIRECTORY/root/etc/pacman.conf"
-        systemd-machine-id-setup --root="$BUILD_DIRECTORY/root"
-        init_archlinux
-    fi
-}
-
-install_user(){
-    exec_nspawn root useradd -m -G wheel -s /bin/bash build
-    echo "build ALL = NOPASSWD: /usr/bin/pacman" > $BUILD_DIRECTORY/root/etc/sudoers.d/build-pacman
-}
-
-install_pacman-git(){
-    mkdir -p $BUILD_DIRECTORY/root/home/build/pacman
-    curl -o $BUILD_DIRECTORY/root/home/build/pacman/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacman-git
-
-    echo 'provides=("pacman=5.0.2")' >> $BUILD_DIRECTORY/root/home/build/pacman/PKGBUILD
-    exec_nspawn root chown -R build:build /home/build/pacman
-    exec_nspawn root sudo -iu build bash -c 'cd pacman; makepkg --noconfirm -si'
-    # Pacman replaces these files, so lets reinsert them
-    cp "./makepkg.conf" "$BUILD_DIRECTORY/root/etc/makepkg.conf"
-    cp "./pacman.conf" "$BUILD_DIRECTORY/root/etc/pacman.conf"
-}
-
-init_archlinux(){
-    exec_nspawn root pacman -Syu --noconfirm
-    exec_nspawn root pacman -S base base-devel --noconfirm
-    exec_nspawn root locale-gen
-}
-
-if [ ! -f $BOOTSTRAP_IMG ]; then
-    curl -O  $BOOTSTRAP_MIRROR/$BOOTSTRAP_IMG
+if [ ! -f "$bootstrap_img" ]; then
+    curl -O "$bootstrap_mirror/$bootstrap_img"
 fi
 
-init_container $BOOTSTRAP_IMG
-install_user
-install_pacman-git
-exec_nspawn root bash
+if [ ! -d "$build_directory" ]; then
+    mkdir -p $build_directory
+fi
 
+# Prepare root chroot
+if [ ! -d "$build_directory"/root ]; then
+    tar xvf "$bootstrap_img" -C "$build_directory" &> /dev/null
+    mv "$build_directory"/root.* "$build_directory"/root
+
+    # host_mirror=$(curl -s 'https://www.archlinux.org/mirrorlist/?protocol=https' | awk '/^#Server/ {print $3; exit}')
+    ## Hardcoded until further notice
+    host_mirror="http://mirror.neuf.no/archlinux/\$repo/os/\$arch"
+
+    printf 'Server = %s\n' "$host_mirror" > "$build_directory"/root/etc/pacman.d/mirrorlist
+    printf '%s.UTF-8 UTF-8\n' en_US de_DE > "$build_directory"/root/etc/locale.gen
+    printf 'LANG=en_US.UTF-8\n' > "$build_directory"/root/etc/locale.conf
+
+    cp ./makepkg.conf "$build_directory"/root/etc/makepkg.conf
+    cp ./pacman.conf "$build_directory"/root/etc/pacman.conf
+
+    systemd-machine-id-setup --root="$build_directory"/root
+
+    exec_nspawn root pacman-key --init &> /dev/null
+    exec_nspawn root pacman-key --populate archlinux &> /dev/null
+    exec_nspawn root pacman-key --refresh &> /dev/bull
+    
+    exec_nspawn root pacman -Syu --noconfirm --ignore linux --ignore mkinitcpio
+    exec_nspawn root pacman -S base base-devel --noconfirm
+    exec_nspawn root locale-gen
+
+    printf 'build ALL = NOPASSWD: /usr/bin/pacman\n' > "$build_directory"/root/etc/sudoers.d/build-pacman
+    exec_nspawn root useradd -m -G wheel -s /bin/bash build
+
+    mkdir -p "$build_directory"/root/home/build/pacman
+
+    curl -o "$build_directory"/root/home/build/pacman/PKGBUILD https://raw.githubusercontent.com/Earnestly/pkgbuilds/master/pacman-git/PKGBUILD
+
+    exec_nspawn root chown -R build:build /home/build/pacman
+    exec_nspawn root sudo -iu build bash -c 'cd pacman && makepkg --noconfirm -csrif'
+    cp ./makepkg.conf "$build_directory"/root/etc/makepkg.conf
+    cp ./pacman.conf "$build_directory"/root/etc/pacman.conf
+fi
+
+
+
+
+exec_nspawn root bash
