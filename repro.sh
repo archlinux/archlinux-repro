822f711cb1c0770421562bf485b07e2c34dd70f8
Morten Linderud <morten@linderud.pw>
2017-12-11 22:57:14 +0100

    Restructure, added traps (i think) and rsync support
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- repro.sh
+++ repro.sh
@@ -2,6 +2,41 @@
 
 # Misc functions i want from pacman and makepkg
 
+set -eE -o pipefail
+
+readonly build_directory=/var/lib/repro
+readonly bootstrap_mirror=https://mirror.archlinux.no/iso/latest
+readonly bootstrap_img=archlinux-bootstrap-"$(date +%Y.%m)".01-"$(uname -m)".tar.gz
+
+readonly config_dir=/home/fox/Git/prosjekter/Bash/devtools-repro
+
+pacman_conf=$config_dir/pacman.conf
+makepkg_conf=$config_dir/makepkg.conf
+
+# img_directory=$(mktemp -dt .arch_img)
+img_directory="/tmp/arch_img"
+mkdir -p /tmp/arch_img
+
+
+
+orig_argv=("$0" "$@")
+check_root() {
+	(( EUID == 0 )) && return
+	if type -P sudo >/dev/null; then
+		exec sudo -- "${orig_argv[@]}"
+	else
+		exec su root -c "$(printf ' %q' "${orig_argv[@]}")"
+	fi
+}
+
+is_btrfs() {
+	[[ -e "$1" && "$(stat -f -c %T "$1")" == btrfs ]]
+}
+
+is_subvolume() {
+	[[ -e "$1" && "$(stat -f -c %T "$1")" == btrfs && "$(stat -c %i "$1")" == 256 ]]
+}
+
 colorize() {
 	# prefer terminal safe colored and bold text when tput is supported
 	if tput setaf 0 &>/dev/null; then
@@ -49,42 +84,55 @@ error() {
 }
 
 
-cleanup_root_volume(){
-    warning "removing root container..."
-    btrfs subvolume delete "$build_directory/root" > /dev/null
-}
-
-cleanup_snapshot(){
-    local build=$1
-    warning "removing $build container..."
-    btrfs subvolume delete "$build_directory/$build" > /dev/null || true
-}
-
-readonly build_directory=/var/lib/repro
-readonly config_dir=/home/fox/Git/prosjekter/Bash/devtools-repro
-readonly bootstrap_mirror=https://mirror.archlinux.no/iso/latest
-readonly bootstrap_img=archlinux-bootstrap-"$(date +%Y.%m)".01-"$(uname -m)".tar.gz
 
 exec_nspawn(){
     local container=$1
     systemd-nspawn -q --as-pid2 -D "$build_directory/$container" "${@:2}"
 }
 
-# $1 -> name of container
-create_snapshot (){
-    local build=$1
-    exec_nspawn root pacman -Syu --noconfirm
-    msg "Create snapshot for $build..."
-    btrfs subvolume snapshot "$build_directory/root" "$build_directory/$build" &> /dev/null
-    touch "$build_directory/$build"
+cleanup_root_volume(){
+    warning "Removing root container..."
+    if is_subvolume "$build_directory/$build"; then 
+        btrfs subvolume delete "$build_directory/root" &> /dev/null
+    else
+        rm -rf "$build_directory/root"
+    fi
 }
 
 # $1 -> name of container
 remove_snapshot (){
     local build=$1
-    msg "Delete snapshot for $build..."
-    btrfs subvolume delete "$build_directory/$build" &> /dev/null
+    msg2 "Delete snapshot for $build..."
+    if is_subvolume "$build_directory/$build"; then 
+        btrfs subvolume delete "$build_directory/$build" &> /dev/null
+    else
+        rm -rf "$build_directory/$build"
+    fi
 }
+
+# $1 -> name of container
+create_snapshot (){
+    local build=$1
+
+    trap '{ remove_snapshot $build ; exit 1; }' ERR
+    trap '{ remove_snapshot $build ; trap - INT; kill -INT $$; }' INT
+
+    msg2 "Create snapshot for $build..."
+    if [ ! -z ${use_rsync+x} ] || ! is_btrfs "$build_directory/root"; then 
+        msg2 "Creating rsync snapshot"
+        mkdir -p "$build_directory/$build"
+		rsync -a --delete -W -x "$build_directory/root/" "$build_directory/$build"
+    else
+        msg2 "Creating btrfs snapshot"
+        if ! btrfs subvolume snapshot "$build_directory/root" "$build_directory/$build" &> /dev/null; then
+            error "Couldn't create snapshot, did you forget -R?"
+            remove_snapshot $build && exit 1
+        fi
+    fi
+
+    touch "$build_directory/$build"
+}
+
 
 build_package(){
     local build=$1
@@ -98,7 +146,7 @@ chown build:build /pkgdest
 mkdir -p /srcpkgdest
 chown build:build /srcpkgdest
 cd /startdir
-sudo -u build SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH PKGDEST=/pkgdest SRCPKGDEST=/srcpkgdest makepkg --syncdeps --noconfirm --skipinteg || true
+sudo -u build SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH PKGDEST=/pkgdest SRCPKGDEST=/srcpkgdest makepkg -sc --noconfirm || true
 __END__
     mkdir -p "./$build" || true
     for pkgfile in "$build_directory/$build"/pkgdest/*; do
@@ -106,87 +154,138 @@ __END__
     done
 }
 
-set -e
+init_chroot(){
+    set -e
 
-if [ ! -f "$build_directory/$bootstrap_img" ]; then
-    curl -o  "$build_directory/$bootstrap_img" "$bootstrap_mirror/$bootstrap_img"
-fi
+    check_root
 
-if [ ! -d "$build_directory" ]; then
-    mkdir -p $build_directory
-fi
-
-# Prepare root chroot
-if [ ! -d "$build_directory"/root ]; then
-    trap '{ cleanup_root_volume; exit 1; }' ERR
-    msg "Preparing chroot"
-    msg2 "Extracting image into container..."
-    btrfs subvolume create "$build_directory/root" > /dev/null
-    tar xvf "$build_directory/$bootstrap_img" -C "$build_directory/root" --strip-components=1 > /dev/null
-
-    # host_mirror=$(curl -s 'https://www.archlinux.org/mirrorlist/?protocol=https' | awk '/^#Server/ {print $3; exit}')
-    ## Hardcoded until further notice
-    host_mirror="http://mirror.neuf.no/archlinux/\$repo/os/\$arch"
-
-    printf 'Server = %s\n' "$host_mirror" > "$build_directory"/root/etc/pacman.d/mirrorlist
-    printf '%s.UTF-8 UTF-8\n' en_US de_DE > "$build_directory"/root/etc/locale.gen
-    printf 'LANG=en_US.UTF-8\n' > "$build_directory"/root/etc/locale.conf
-
-    cp $config_dir/makepkg.conf "$build_directory"/root/etc/makepkg.conf
-    cp $config_dir/pacman.conf "$build_directory"/root/etc/pacman.conf
-
-    systemd-machine-id-setup --root="$build_directory"/root
-    msg2 "Setting up keyring, this might take a while..."
-    # exec_nspawn root pacman-key --init #&> /dev/null
-    # exec_nspawn root pacman-key --populate archlinux #&> /dev/null
-    # exec_nspawn root pacman-key --refresh #&> /dev/bull
-    
-    msg2 "Updating and installing base & base-devel"
-    exec_nspawn root pacman -Syu --noconfirm --ignore linux --ignore pacman-git
-    exec_nspawn root pacman -S base base-devel --noconfirm --ignore pacman-git
-    exec_nspawn root locale-gen
-
-    printf 'build ALL = NOPASSWD: /usr/bin/pacman\n' > "$build_directory"/root/etc/sudoers.d/build-pacman
-    exec_nspawn root useradd -m -G wheel -s /bin/bash build
-
-    msg2 "Installing pacman-git"
-    exec_nspawn root bash -c "yes | pacman -S pacman-git"
-    cp $config_dir/makepkg.conf "$build_directory"/root/etc/makepkg.conf
-    cp $config_dir/pacman.conf "$build_directory"/root/etc/pacman.conf
-fi
-
-SOURCE_DATE_EPOCH=$(date +%s)
-msg "Using SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
-
-# trap '{ cleanup_snapshot; exit 1; }' ERR
-
-# Build 1
-msg2 "Starting build1..."
-create_snapshot "build1"
-build_package "build1"
-remove_snapshot "build1"
-
-# Build 2
-msg2 "Starting build2..."
-create_snapshot "build2"
-build_package "build2"
-remove_snapshot "build2"
-
-msg "Comparing hashes..."
-is_error=false
-for pkgfile in ./build1/*; do
-    sha512sum -b $pkgfile | read build1_checksum _
-    sha512sum -b $pkgfile | read build2_checksum _
-    if [ "$build1_checksum" = "$build2_checksum" ]; then
-      msg2 "${pkgfile##*/} is reproducible!"
-    else
-      warning "${pkgfile##*/} is not reproducible!"
-      is_error=true
+    if [ ! -d "$build_directory" ]; then
+        mkdir -p $build_directory
     fi
+
+    # Prepare root chroot
+    if [ ! -d "$build_directory"/root ]; then
+
+        msg "Preparing chroot"
+        trap '{ cleanup_root_volume; exit 1; }' ERR
+        trap '{ cleanup_root_volume; trap - INT; kill -INT $$; }' INT
+
+        msg2 "Extracting image into container..."
+        mkdir -p $build_directory/root
+        # Turn into a subvolume if there is btrfs present
+        if [ -z ${use_rsync+x} ] || ! is_btrfs "$build_directory"; then 
+            msg "Creating btrfs snapshot"
+            btrfs subvolume create "$build_directory/root" > /dev/null
+        fi
+        tar xvf "$img_directory/$bootstrap_img" -C "$build_directory/root" --strip-components=1 > /dev/null
+
+        # host_mirror=$(curl -s 'https://www.archlinux.org/mirrorlist/?protocol=https' | awk '/^#Server/ {print $3; exit}')
+        ## Hardcoded until further notice
+        host_mirror="http://mirror.neuf.no/archlinux/\$repo/os/\$arch"
+
+        printf 'Server = %s\n' "$host_mirror" > "$build_directory"/root/etc/pacman.d/mirrorlist
+        printf '%s.UTF-8 UTF-8\n' en_US de_DE > "$build_directory"/root/etc/locale.gen
+        printf 'LANG=en_US.UTF-8\n' > "$build_directory"/root/etc/locale.conf
+
+        echo $makepkg_conf
+        echo $pacman_conf
+
+        cp $makepkg_conf "$build_directory"/root/etc/makepkg.conf
+        cp $pacman_conf "$build_directory"/root/etc/pacman.conf
+
+        systemd-machine-id-setup --root="$build_directory"/root
+        msg2 "Setting up keyring, this might take a while..."
+        # exec_nspawn root pacman-key --init #&> /dev/null
+        # exec_nspawn root pacman-key --populate archlinux #&> /dev/null
+        # exec_nspawn root pacman-key --refresh #&> /dev/bull
+        
+        msg2 "Updating and installing base & base-devel"
+        exec_nspawn root pacman -Syu --noconfirm --ignore pacman-git
+        exec_nspawn root pacman -S base-devel --noconfirm --ignore pacman-git
+        exec_nspawn root locale-gen
+
+        printf 'build ALL = NOPASSWD: /usr/bin/pacman\n' > "$build_directory"/root/etc/sudoers.d/build-pacman
+        exec_nspawn root useradd -m -G wheel -s /bin/bash build
+
+        msg2 "Installing pacman-git"
+        exec_nspawn root bash -c "yes | pacman -S pacman-git"
+        cp $makepkg_conf "$build_directory"/root/etc/makepkg.conf
+        cp $pacman_conf "$build_directory"/root/etc/pacman.conf
+    fi
+}
+
+cmd_build(){
+
+    check_root
+
+    while getopts :C:M:R arg; do
+        case $arg in
+            C) pacman_conf=$OPTARG;;
+            M) makepkg_conf=$OPTARG;;
+            *);;
+        esac
+    done
+
+    SOURCE_DATE_EPOCH=$(date +%s)
+    msg "Using SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
+
+    # trap '{ cleanup_snapshot; exit 1; }' ERR
+    
+    exec_nspawn root pacman -Syu --noconfirm
+
+    # Build 1
+    msg "Starting build1..."
+    create_snapshot "build1"
+    build_package "build1"
+    remove_snapshot "build1"
+
+    # Build 2
+    msg "Starting build2..."
+    create_snapshot "build2"
+    build_package "build2"
+    remove_snapshot "build2"
+
+    msg "Comparing hashes..."
+    is_error=false
+    for pkgfile in ./build1/*; do
+        sha512sum -b $pkgfile | read build1_checksum _
+        sha512sum -b $pkgfile | read build2_checksum _
+        if [ "$build1_checksum" = "$build2_checksum" ]; then
+          msg2 "${pkgfile##*/} is reproducible!"
+        else
+          warning "${pkgfile##*/} is not reproducible!"
+          is_error=true
+        fi
+    done
+
+    if $is_error; then
+        error "Package is not reproducible"
+    else
+        msg "Package is reproducible!"
+    fi
+}
+
+cmd_check(){
+    echo "Doing a build!"
+}
+
+cmd_help(){
+    echo "Help string"
+}
+
+if [ ! -d "/tmp/arch_img" ]; then
+    curl -o  "$img_directory/$bootstrap_img" "$bootstrap_mirror/$bootstrap_img"
+fi
+
+while getopts :R arg; do
+    case $arg in
+        R) use_rsync=true;;
+        *);;
+    esac
 done
 
-if $is_error; then
-    error "Package is not reproducible"
-else
-    msg "Package is reproducible!"
-fi
+case "$1" in
+    help) shift; cmd_help "$@" ;;
+    check) shift; init_chroot; cmd_check "$@" ;;
+    *) init_chroot; cmd_build "$@" ;;
+esac
